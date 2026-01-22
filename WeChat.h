8367ad7d5c72948bedefe4f5b51ba587dc3845ee
shilyx <shilyx@outlook.com>
2026-01-22 09:38:19 +0800

    wechat 4.0

--- WeChat.h
+++ WeChat.h
@@ -4,9 +4,9 @@
 #include <Windows.h>
 #include <AclAPI.h>
 
-static void EnableMultiWeChat()
+static void CreateDenyMutexW(LPCWSTR lpMutextName)
 {
-    HANDLE hMutex = CreateMutexW(NULL, FALSE, L"_WeChat_App_Instance_Identity_Mutex_Name");
+    HANDLE hMutex = CreateMutexW(NULL, FALSE, lpMutextName);
     SID_IDENTIFIER_AUTHORITY SIDAuthWorld = SECURITY_WORLD_SID_AUTHORITY;
     PSID pEveryoneSID = NULL; // everyoneÈº×éSID
     char szBuffer[4096];
@@ -22,6 +22,14 @@ static void EnableMultiWeChat()
     InitializeAcl(pAcl, sizeof(szBuffer), ACL_REVISION);
     AddAccessDeniedAce(pAcl, ACL_REVISION, MUTEX_ALL_ACCESS, pEveryoneSID);
     SetSecurityInfo(hMutex, SE_KERNEL_OBJECT, DACL_SECURITY_INFORMATION, NULL, NULL, pAcl, NULL);
+}
+
+// _WeChat_App_Instance_Identity_Mutex_Name 3.0
+// XWeChat_App_Instance_Identity_Mutex_Name 4.0
+static void EnableMultiWeChat()
+{
+    CreateDenyMutexW(L"_WeChat_App_Instance_Identity_Mutex_Name");
+    CreateDenyMutexW(L"XWeChat_App_Instance_Identity_Mutex_Name");
 }
 
 #endif /* _WECHAT_H */
