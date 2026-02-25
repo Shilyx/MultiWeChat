15a0279da2cd81380ccdb1e0d4085ca30c491362
shilyx <shilyx@outlook.com>
2026-02-25 18:19:23 +0800

    tray

--- MultiWeChat/resource.h
+++ MultiWeChat/resource.h
@@ -1,16 +1,15 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
+// Microsoft Visual C++ generated include file.
 // Used by MultiWeChat.rc
 //
 #define IDI_MAINFRAME                   101
 #define IDD_DIALOG                      102
-#define IDR_241                         103
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
